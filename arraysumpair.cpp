be44e901022fea63a77c8da6923e2d8a12421b97
sreesurendran <sreesurendran@users.noreply.github.com>
2014-02-03 22:15:29 -0600

    Update arraysumpair.cpp

--- arraysumpair.cpp
+++ arraysumpair.cpp
@@ -1,3 +1,9 @@
+//For an unosrted array, given a value k, list all the pairs of numbers that sum upto k
+//No duplicates
+//Complexity: O(n)
+//I/P format: ./a.out <k> <array_separated_by_space>
+//eg.: ./a.out 6 3 7 1 2 3 4 5 1 -1 7 3
+
 #include <iostream>
 #include <unordered_map>
 #include <vector>
